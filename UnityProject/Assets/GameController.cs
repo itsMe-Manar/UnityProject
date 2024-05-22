c8b4665144594acee41a261e155b4597ea5d21c0
itsMe-Manar <chakirmanar@gmail.com>
2024-05-22 21:43:01 +0200

    Manar everything except boundaries

---
+++ UnityProject/Assets/GameController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    Vector2 startPos;
+    SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();    
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+   private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Obstacle"))
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        StartCoroutine(Respawn(0.5f));  
+    }
+
+    IEnumerator Respawn(float duration)
+    {
+        spriteRenderer.enabled = false;
+        yield return new WaitForSeconds(duration);  
+        transform.position = startPos;  
+        spriteRenderer.enabled = true;  
+    }
+}
